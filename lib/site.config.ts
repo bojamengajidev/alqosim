2052da30bb892286c723e48ca9c19085b0a6f4bc
ArRasyid99 <arrasyidyudha@students.unnes.ac.id>
2026-03-21 03:12:58 +0700

    update

--- lib/site.config.ts
+++ lib/site.config.ts
@@ -7,5 +7,5 @@ type SiteConfig = {
 export const siteConfig: SiteConfig = {
   site_name: "Masjid Muhammad Al Qosim",
   site_description: "Yayasan Kajian Boja Mengaji",
-  site_domain: "https://next-wp.com",
+  site_domain: "https://masjidalqosim.com",
 };
